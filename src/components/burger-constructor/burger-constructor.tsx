aedaf883870bee869abd71ae3d77e0b41f8b6367
AleksandrSakhno <ansakhno1984@yandex.ru>
2023-02-01 12:12:48 +0300

    добавил подсчет итоговой стоимости

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -12,6 +12,10 @@ import { data } from '../../utils/data';
 
 const BurgerConstructor: FC = () => {
   const datas = data.slice(0, data.length / 2);
+  const totalPrice = () =>
+    datas.reduce((acc, item) => {
+      return (acc = acc + item.price);
+    }, 0);
 
   const ingredients = () => {
     return datas.map((item, index) => {
@@ -37,10 +41,10 @@ const BurgerConstructor: FC = () => {
     <div className={`${styles.container} mt-25`}>
       <div className={`${styles.item} mb-4 pl-8`}>
         <ConstructorElement
-          key={data[0]._id}
-          price={data[0].price}
-          text={data[0].name}
-          thumbnail={data[0].image_mobile}
+          key={datas[0]._id}
+          price={datas[0].price}
+          text={datas[0].name}
+          thumbnail={datas[0].image_mobile}
           type={'top'}
           isLocked
         />
@@ -50,10 +54,10 @@ const BurgerConstructor: FC = () => {
 
       <div className={`${styles.item} mb-4 pl-8`}>
         <ConstructorElement
-          key={data[data.length - 1]._id}
-          price={data[data.length - 1].price}
-          text={data[data.length - 1].name}
-          thumbnail={data[data.length - 1].image_mobile}
+          key={datas[datas.length - 1]._id}
+          price={datas[datas.length - 1].price}
+          text={datas[datas.length - 1].name}
+          thumbnail={datas[datas.length - 1].image_mobile}
           type={'bottom'}
           isLocked
         />
@@ -63,7 +67,7 @@ const BurgerConstructor: FC = () => {
           Оформить заказ
         </Button>
         <span className={`${styles.totalPrice} text text_type_digits-medium`}>
-          <CurrencyIcon type='primary' /> 610
+          <CurrencyIcon type='primary' /> {totalPrice()}
         </span>
       </div>
     </div>
